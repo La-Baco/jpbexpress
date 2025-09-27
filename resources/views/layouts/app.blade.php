8c3809347c7c5d54e4b610bb1170b5d46d8d6d9a
La-Baco <labaco.aco@gmail.com>
2025-09-28 01:09:20 +0700

    update dikit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,7 +89,7 @@
                 <li class="nav-item navbar-dropdown dropdown-user dropdown">
                   <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
-<img src="{{ Auth::user()->foto ? asset('storage/foto_user/' . Auth::user()->foto) : asset('assets/img/avatars/1.png') }}"
+<img src="{{ Auth::user()->foto ? asset('storage/foto_user/' . Auth::user()->foto) : asset('assets/template2/img/default.png') }}"
         alt="Foto Profil" class="w-px-40 h-auto rounded-circle" />                    </div>
                   </a>
                   <ul class="dropdown-menu dropdown-menu-end">
@@ -98,7 +98,7 @@
                         <div class="d-flex">
                           <div class="flex-shrink-0 me-3">
                             <div class="avatar avatar-online">
-<img src="{{ Auth::user()->foto ? asset('storage/foto_user/' . Auth::user()->foto) : asset('assets/img/avatars/1.png') }}"
+<img src="{{ Auth::user()->foto ? asset('storage/foto_user/' . Auth::user()->foto) : asset('assets/template2/img/default.png') }}"
         alt="Foto Profil" class="w-px-40 h-auto rounded-circle" />                            </div>
                           </div>
                           <div class="flex-grow-1">
